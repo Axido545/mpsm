bb630d81d3d9fa375b1f94a7758220af348ca927
axido545 <axido545@gmail.com>
2024-06-23 19:40:37 +0200

    add data

---
+++ data/index.ts
@@ -0,0 +1,7 @@
+export const gridItems = [
+    {title : 'title1', description:'Description1', id: '1'},
+    {title : 'title1', description:'Description1', id: '1'},
+    {title : 'title1', description:'Description1', id: '1'},
+    {title : 'title1', description:'Description1', id: '1'},
+    {title : 'title1', description:'Description1', id: '1'},
+]
