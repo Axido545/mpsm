35c991981e9e5cf3d058941a9295b0a409af0193
axido545 <axido545@gmail.com>
2024-07-13 20:52:20 +0200

    add click on btn

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -20,7 +20,7 @@ function MagicButton({
      className={`relative inline-flex h-12 
     w-full 
     overflow-hidden rounded-lg p-[1px] 
-    focus:outline-none md:w-60 md:mt-10  ${otherClasses}`}
+    focus:outline-none md:w-60 md:mt-10`} onClick={handleClick}
     >
       {/* change inset-[-1000%]  to inset-0  animate-[spin_2s_linear_infinite] to  */}
     <span className="absolute inset-0
@@ -30,7 +30,7 @@ function MagicButton({
     <span className={`relative inline-flex h-full w-full
      cursor-pointer items-center justify-center 
      rounded-lg bg-slate-950 px-7  text-sm font-medium
-      text-white backdrop-blur-3xl gap-2`}>
+      text-white backdrop-blur-3xl gap-2 ${otherClasses}`}>
         {position === 'left' && icon }
       {title}
       {position === 'right' && icon }
