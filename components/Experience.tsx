c02eeddda43d47a2a72ab7a2f37d37ef03071033
axido545 <axido545@gmail.com>
2024-09-20 23:04:33 +0200

    add exp

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,9 +1,32 @@
+import { companies, workExperience } from '@/data'
 import React from 'react'
-////0206:35
+////020810
 const Experience = () => {
     return (
-        <div>
-Experience
+        <div >
+            <div className='flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg mt-10'>
+                {companies.map(({id,img,name, nameImg}) => (
+                    <div key={id} className='flex md:max-w-6 max-w-32 gap-2'>
+                   < img src={img} alt={name} className="md:w-10 w-5"/>
+                   < img src={nameImg} alt={name} className="md:w-24 w-20"/>
+
+                    </div>
+                ))}
+            </div>
+            <div className='py-20' id='projects'>
+      <h1 className="heading">
+     My
+      <span className='text-purple'>work experience
+      </span>
+      </h1>
+      <div className='w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10'>
+        {workExperience.map((card) =>(
+            <Button >
+        
+            </Button>
+        ))}
+      </div>
+      </div>
         </div>
     )
 }
